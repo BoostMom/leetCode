0b14babf5e3457409dc7111c2f0804ac462dc53d
BoostMom <734140578@qq.com>
2017-04-21 16:24:13 +0800

    java leetCode project

---
+++ LeetCode/src/boostMom/MaxConsOnes.java
@@ -0,0 +1,11 @@
+package boostMom;
+
+public class MaxConsOnes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a test Git");
+
+	}
+
+}
